d872394b036dae349595b34ee8e6f2d56380cf8f
Phaedon Sinis <phaedonsinis@gmail.com>
2011-10-28 15:05:42 -0700

    VERY minimal test program is finally running!

--- src/ohiio.cpp
+++ src/ohiio.cpp
@@ -1,249 +1,39 @@
 #include "ohiio.h"
 #include "imageio.h"
-#include "ctypedesc.h"
-#include "typedesc.h"
-#include "paramlist.h"
-#include "cparamlist.h"
+    
 
-    
-    /****************************
-     *
-     *  class QuantizationSpec
-     *
-     ****************************/
-    
-    
-    int QSpecCreate_0 (QuantizationSpec **qsptr,
-                       int _black, 
-                       int _white, 
-                       int _min, 
-                       int _max) {
-        *qsptr = new QuantizationSpec(_black, _white, _min, _max) ;
-        return 0;
+
+
+/****************************
+ *
+ *  class ImageSpec
+ *
+ ****************************/
+
+
+/// Constructor: given just the data format, set the default quantize
+/// and set all other channels to something reasonable.
+ImageSpec *ImageSpecCreate_0 (TypeDesc *format) {
+    if (!format) {
+        return new ImageSpec();
+    } else {
+        return new ImageSpec(*format);
     }
+}
+
+/// Constructor for simple 2D scanline image with nothing special.
+/// If fmt is not supplied, default to unsigned 8-bit data.
+ImageSpec *ImageSpecCreate_1 (int xres, int yres, int nchans, 
+                              TypeDesc *fmt) {
     
-    int QSpecCreate_1 (QuantizationSpec **qsptr,
-                       CTypeDesc *_type) {
-        
-        *qsptr = new QuantizationSpec( *(TypeDesc *)_type );
-        return 0;
+    if (!fmt) {
+        return new ImageSpec(xres, yres, nchans);
+    } else {
+        return new ImageSpec(xres, yres, nchans, *fmt);
     }
+}
     
-    
-    
-    
-    /****************************
-     *
-     *  class ImageSpec
-     *
-     ****************************/
-    
-    int ImageSpecCreate_0 (CImageSpec **isptr,
-                           CTypeDesc *format) {
-        
-        *isptr = (CImageSpec *) new ImageSpec( *(TypeDesc *)format);
-        return 0;
-    }
-    
-    int ImageSpecCreate_1 (CImageSpec **isptr,
-                           int xres, int yres, 
-                           int nchans, 
-                           CTypeDesc *fmt) {
-        
-        *isptr = (CImageSpec *) new ImageSpec(xres, yres, nchans, *(TypeDesc *)fmt);
-        return 0;
-    }
-    
-    void ImageSpec_set_format (CImageSpec *imageSpec,
-                               CTypeDesc *fmt) {
-        ((ImageSpec *)imageSpec)->set_format( *(TypeDesc *)fmt);
-        
-    }
-    
-    void ImageSpec_default_channel_names (CImageSpec *imageSpec) {
-        ((ImageSpec *)imageSpec)->default_channel_names();
-    }
-    
-    
-    CTypeDesc *format_from_quantize (CImageSpec *imageSpec,
-                                   int quant_black, int quant_white,
-                                   int quant_min, int quant_max) {
-        
-        CTypeDesc *newTypeDesc = (CTypeDesc *)malloc (sizeof(CTypeDesc));
-        TypeDesc tDesc = ((ImageSpec *)imageSpec)->format_from_quantize(quant_black, quant_white, quant_min, quant_max);
-        memcpy(newTypeDesc, &tDesc, sizeof(TypeDesc));
-        return newTypeDesc;
-    }
-    
-    size_t ImageSpec_channel_bytes_0 (const CImageSpec *imageSpec) {
-        return ((ImageSpec *)imageSpec)->channel_bytes();
-    }
-    
-    
-    size_t ImageSpec_channel_bytes_1 (const CImageSpec *imageSpec,
-                                      int chan, bool native) {
-        return ((ImageSpec *)imageSpec)->channel_bytes(chan, native);
-    }
-    
-    size_t ImageSpec_pixel_bytes_0 (const CImageSpec *imageSpec,
-                                    bool native) {
-        return ((ImageSpec *)imageSpec)->pixel_bytes(native);
-    }
-    
-    size_t ImageSpec_pixel_bytes_1 (const CImageSpec *imageSpec,
-                                    int firstchan, int nchans, bool native) {
-        return ((ImageSpec *)imageSpec)->pixel_bytes(firstchan, nchans, native);
-    }
-    
-    imagesize_t ImageSpec_scanline_bytes (const CImageSpec *imageSpec,
-                                          bool native) {
-        return ((ImageSpec *)imageSpec)->scanline_bytes(native);
-    }
-    
-    imagesize_t ImageSpec_tile_pixels (const CImageSpec *imageSpec) {
-        return ((ImageSpec *)imageSpec)->tile_pixels();
-    }
-    
-    imagesize_t ImageSpec_tile_bytes (const CImageSpec *imageSpec,
-                                      bool native) {
-        return ((ImageSpec *)imageSpec)->tile_bytes(native);
-    }
-    
-    imagesize_t ImageSpec_image_pixels (const CImageSpec *imageSpec) {
-        return ((ImageSpec *)imageSpec)->image_pixels();
-    }
-    
-    imagesize_t ImageSpec_image_bytes (const CImageSpec *imageSpec,
-                                       bool native) {
-        return ((ImageSpec *)imageSpec)->image_bytes(native);
-    }
-    
-    bool ImageSpec_size_t_safe(const CImageSpec *imageSpec) {
-        return ((ImageSpec *)imageSpec)->size_t_safe();
-    }
-    
-    
-    void ImageSpec_auto_stride_0 (CImageSpec *imageSpec,
-                                  stride_t *xstride, stride_t *ystride,
-                                  stride_t *zstride, stride_t channelsize,
-                                  int nchannels, int width, int height) {
-        ((ImageSpec *)imageSpec)->auto_stride(*xstride, *ystride, *zstride, channelsize, nchannels, width, height);
-    }
-    
-    void ImageSpec_auto_stride_1 (CImageSpec *imageSpec,
-                                  stride_t *xstride, stride_t *ystride,
-                                  stride_t *zstride, CTypeDesc *format,
-                                  int nchannels, int width, int height) {
-        ((ImageSpec *)imageSpec)->auto_stride(*xstride, *ystride, *zstride, *(TypeDesc *)format, nchannels, width, height);
-    }
-    
-    void ImageSpec_auto_stride_2 (CImageSpec *imageSpec,
-                                  stride_t *xstride, CTypeDesc *format, int nchannels) {
-        ((ImageSpec *)imageSpec)->auto_stride(*xstride, *(TypeDesc *)format, nchannels);
-    }
-    
-    void ImageSpec_attribute_0 (CImageSpec *imageSpec,
-                                const char *name, CTypeDesc *type, const void *value) {
-        ((ImageSpec *)imageSpec)->attribute(name, *(TypeDesc *)type, value);
-    }
-    
-    void ImageSpec_attribute_1 (CImageSpec *imageSpec,
-                                const char *name, CTypeDesc *type, const char *value) {
-        ((ImageSpec *)imageSpec)->attribute(name, *(TypeDesc *)type, value);
-    }
-    
-    void ImageSpec_attribute_2 (CImageSpec *imageSpec,
-                                const char *name, unsigned int value) {
-        ((ImageSpec *)imageSpec)->attribute(name, value);
-    }
-    
-    void ImageSpec_attribute_3 (CImageSpec *imageSpec,
-                                const char *name, int value) {
-        ((ImageSpec *)imageSpec)->attribute(name, value);
-    }
-    
-    void ImageSpec_attribute_4 (CImageSpec *imageSpec,
-                                const char *name, float value) {
-        ((ImageSpec *)imageSpec)->attribute(name, value);
-    }
-    
-    void ImageSpec_attribute_5 (CImageSpec *imageSpec,
-                                const char *name, const char *value) {
-        ((ImageSpec *)imageSpec)->attribute(name, value);
-    }
-    
-    void ImageSpec_attribute_6 (CImageSpec *imageSpec,
-                                const char *name, const char *value) {
-        ((ImageSpec *)imageSpec)->attribute(name, value);
-    }
-    
-    void ImageSpec_erase_attribute (CImageSpec *imageSpec,
-                                    const char *name,
-                                    CTypeDesc *searchtype,
-                                    bool casesensitive) {
-        ((ImageSpec *)imageSpec)->erase_attribute(name, *(TypeDesc *)searchtype, casesensitive);
-    }
-    
- /*   CImageIOParameter * find_attribute (CImageSpec *imageSpec,
-                                       const char *name,
-                                       CTypeDesc *searchtype,
-                                       bool casesensitive) {
-        
-        
-        
-        return ((ImageSpec *)imageSpec)->find_attribute(name, *(TypeDesc *)searchtype, casesensitive);
-    }
-    
-    const CImageIOParameter *ImageSpec_find_attribute (const CImageSpec *imageSpec,
-                                                      const char *name,
-                                                      CTypeDesc *searchtype,
-                                                      bool casesensitive) {
-        return ((ImageSpec *)imageSpec)->find_attribute(name, searchtype, casesensitive);
-    }
-    */
-    
-    int ImageSpec_get_int_attribute (const CImageSpec *imageSpec,
-                                     const char *name, int defaultval) {
-        return ((ImageSpec *)imageSpec)->get_int_attribute(name, defaultval);
-    }
-    
-    float ImageSpec_get_float_attribute (const CImageSpec *imageSpec,
-                                         const char *name,
-                                         float defaultval) {
-        return ((ImageSpec *)imageSpec)->get_float_attribute(name, defaultval);
-    }
-    
-    const char * ImageSpec_get_string_attribute (const CImageSpec *imageSpec,
-                                                 const char *name,
-                                                 const char *defaultval) {
-        return ((ImageSpec *)imageSpec)->get_string_attribute(name, defaultval).c_str();
-    }
-    
-    const char * ImageSpec_metadata_val (const CImageSpec *imageSpec,
-                                         const CImageIOParameter *p,
-                                         bool human) {
-        return ((ImageSpec *)imageSpec)->metadata_val(*(ImageIOParameter*)p, human).c_str();
-    }
-    
-    const char * ImageSpec_to_xml (const CImageSpec *imageSpec) {
-        return ((ImageSpec *)imageSpec)->to_xml().c_str();
-    }
-    
-    void ImageSpec_from_xml (CImageSpec *imageSpec,
-                             const char *xml) {
-        ((ImageSpec *)imageSpec)->from_xml(xml);
-    }
-    
-    bool ImageSpec_valid_tile_range (CImageSpec *imageSpec,
-                                     int xbegin, int xend, int ybegin, int yend,
-                                     int zbegin, int zend) {
-        return ((ImageSpec *)imageSpec)->valid_tile_range(xbegin, xend, ybegin, yend, zbegin, zend);
-    }
-    
-    
-    
-    
-    
+
     /****************************
      *
      *  class ImageInput
@@ -251,11 +41,10 @@
      ****************************/
     
     
-    int ImageInputCreate (ImageInput **iiptr,
+    void ImageInputCreate (ImageInput **iiptr,
                           char *filename,
                           char *plugin_searchpath) {
         *iiptr = ImageInput::create(filename, plugin_searchpath);
-        return 0;
     }
     
     
@@ -292,14 +81,12 @@
 
     }
 
-
-
     
     bool ImageOutput_open (ImageOutput *imageOutput,
                            const char *name, 
-                           const CImageSpec *newspec,
-                           COpenMode mode) {
-        return imageOutput->open(name, *(ImageSpec*)newspec, (ImageOutput::OpenMode)mode);
+                           const ImageSpec *newspec,
+                           OpenMode mode) {
+        return imageOutput->open(name, *newspec, (ImageOutput::OpenMode)mode);
     }
     
     
--- src/ohiio.h
+++ src/ohiio.h
@@ -11,23 +11,22 @@
 
 
 #ifdef __cplusplus
+
     #include "imageio.h"
-    using namespace OpenImageIO;
+    OIIO_NAMESPACE_USING
+    //using namespace OpenImageIO;
+
 #else
-    #include "stddef.h"
-    #include <stdbool.h>
+    
+    #include <stddef.h> // to get ptrdiff_t
+    #include <stdbool.h> // bool type
+    typedef struct TypeDesc TypeDesc;
+    typedef struct ImageSpec ImageSpec;
     typedef struct ImageInput ImageInput;
     typedef struct ImageOutput ImageOutput;
-    typedef struct QuantizationSpec QuantizationSpec;
-#endif
+    //typedef struct QuantizationSpec QuantizationSpec;
 
-
-struct CParamValue;
-
-typedef ptrdiff_t stride_t;
-typedef struct CParamValue CImageIOParameter;
-
-enum COpenMode { Create, AppendSubimage, AppendMIPLevel };
+    typedef ptrdiff_t stride_t;
 
 /// Type we use to express how many pixels (or bytes) constitute an image,
 /// tile, or scanline.  Needs to be large enough to handle very big images
@@ -36,31 +35,19 @@ enum COpenMode { Create, AppendSubimage, AppendMIPLevel };
 typedef size_t imagesize_t;
 #else
 typedef unsigned long long imagesize_t;
-#endif
+#endif // end LINUX64
 
-struct CTypeDesc;
+#endif // end CPLUSPLUS
 
-struct CImageSpec;
+enum OpenMode { Create, AppendSubimage, AppendMIPLevel };
+
+
 
 
 #ifdef __cplusplus 
 extern "C" {
 #endif
-    /****************************
-     *
-     *  class QuantizationSpec
-     *
-     ****************************/
     
-    int QSpecCreate_0 (QuantizationSpec **qsptr,
-                                   int _black, 
-                                   int _white, 
-                                   int _min, 
-                                   int _max);
-    
-    int QSpecCreate_1 (QuantizationSpec **qsptr, struct CTypeDesc *_type);
-    
-  
     
     /****************************
      *
@@ -70,137 +57,12 @@ extern "C" {
     
     /// Constructor: given just the data format, set the default quantize
     /// and set all other channels to something reasonable.
-    int ImageSpecCreate_0 (struct CImageSpec **isptr,
-                           struct CTypeDesc *format);
-
+    ImageSpec *ImageSpecCreate_0 (TypeDesc *format);
+    
     /// Constructor for simple 2D scanline image with nothing special.
     /// If fmt is not supplied, default to unsigned 8-bit data.
-    int ImageSpecCreate_1 (struct CImageSpec **isptr,
-                           int xres, int yres, 
-                           int nchans, 
-                           struct CTypeDesc *fmt);
-    
-    /// Set the data format, and as a side effect set quantize
-    /// to good defaults for that format
-    void ImageSpec_set_format (struct CImageSpec *imageSpec, 
-                               struct CTypeDesc *fmt);
-    
-    /// Set the channelnames to reasonable defaults ("R", "G", "B", "A"),
-    /// and alpha_channel, based on the number of channels.
-    void ImageSpec_default_channel_names (struct CImageSpec *imageSpec);
-    
-    /// Given quantization parameters, deduce a TypeDesc that can
-    /// be used without unacceptable loss of significant bits.
-    struct CTypeDesc *format_from_quantize (struct CImageSpec *imageSpec,
-                                   int quant_black, int quant_white,
-                                   int quant_min, int quant_max);
-    
-    size_t ImageSpec_channel_bytes_0 (const struct CImageSpec *imageSpec);
-    
-    size_t ImageSpec_channel_bytes_1 (const struct CImageSpec *imageSpec,
-                                      int chan, bool native);
-    
-    size_t ImageSpec_pixel_bytes_0 (const struct CImageSpec *imageSpec, bool native);
-    
-    size_t ImageSpec_pixel_bytes_1 (const struct CImageSpec *imageSpec,
-                                    int firstchan, int nchans, bool native);
-    
-    imagesize_t ImageSpec_scanline_bytes (const struct CImageSpec *imageSpec, bool native);
-    
-    imagesize_t ImageSpec_tile_pixels (const struct CImageSpec *imageSpec);
-    
-    imagesize_t ImageSpec_tile_bytes (const struct CImageSpec *imageSpec, bool native);
-    
-    imagesize_t ImageSpec_image_pixels (const struct CImageSpec *imageSpec);
-    
-    imagesize_t ImageSpec_image_bytes (const struct CImageSpec *imageSpec, bool native);
-    
-    bool ImageSpec_size_t_safe(const struct CImageSpec *imageSpec);
-
-    void ImageSpec_auto_stride_0 (struct CImageSpec *imageSpec,
-                                  stride_t *xstride, stride_t *ystride,
-                             stride_t *zstride, stride_t channelsize,
-                             int nchannels, int width, int height);
-    
-    void ImageSpec_auto_stride_1 (struct CImageSpec *imageSpec,
-                                  stride_t *xstride, stride_t *ystride,
-                                  stride_t *zstride, 
-                                  struct CTypeDesc *format,
-                                  int nchannels, 
-                                  int width, 
-                                  int height);
-    
-    void ImageSpec_auto_stride_2 (struct CImageSpec *imageSpec,
-                                  stride_t *xstride, 
-                                  struct CTypeDesc *format, 
-                                  int nchannels);
-    
-    void ImageSpec_attribute_0 (struct CImageSpec *imageSpec,
-                                const char *name, 
-                                struct CTypeDesc *type, 
-                                const void *value);
-    
-    void ImageSpec_attribute_1 (struct CImageSpec *imageSpec,
-                                const char *name, 
-                                struct CTypeDesc *type, 
-                                const char *value);
-    
-    void ImageSpec_attribute_2 (struct CImageSpec *imageSpec,
-                                const char *name, unsigned int value);
-    
-    void ImageSpec_attribute_3 (struct CImageSpec *imageSpec,
-                                const char *name, int value);
-    
-    void ImageSpec_attribute_4 (struct CImageSpec *imageSpec,
-                                const char *name, float value);
-    
-    void ImageSpec_attribute_5 (struct CImageSpec *imageSpec,
-                                const char *name, const char *value);
-    
-    void ImageSpec_attribute_6 (struct CImageSpec *imageSpec,
-                                const char *name, const char *value);
-    
-    void ImageSpec_erase_attribute (struct CImageSpec *imageSpec,
-                                    const char *name,
-                                    struct CTypeDesc *searchtype,
-                                    bool casesensitive);
-/*
-    CImageIOParameter *find_attribute (CImageSpec *imageSpec,
-                                       const char *name,
-                                       CTypeDesc *searchtype,
-                                       bool casesensitive = false);
-    
-    const CImageIOParameter *ImageSpec_find_attribute (const CImageSpec *imageSpec,
-                                                      const char *name,
-                                                       CTypeDesc *searchtype,
-                                                       bool casesensitive=false);
-  */
-    
-    
-    int ImageSpec_get_int_attribute (const struct  CImageSpec *imageSpec,
-                                     const char *name, int defaultval);
-    
-    float ImageSpec_get_float_attribute (const struct CImageSpec *imageSpec,
-                                         const char *name,
-                                         float defaultval);
-    
-    const char * ImageSpec_get_string_attribute (const struct CImageSpec *imageSpec,
-                                                 const char *name,
-                                                 const char *defaultval);
-
-    const char * ImageSpec_metadata_val (const struct CImageSpec *imageSpec,
-                                         const CImageIOParameter *p,
-                                         bool human);
-    
-    const char * ImageSpec_to_xml (const struct CImageSpec *imageSpec);
-
-    void ImageSpec_from_xml (struct CImageSpec *imageSpec,
-                             const char *xml);
-    
-    bool ImageSpec_valid_tile_range (struct CImageSpec *imageSpec,
-                                     int xbegin, int xend, int ybegin, int yend,
-                                     int zbegin, int zend);
-    
+    ImageSpec *ImageSpecCreate_1 (int xres, int yres, int nchans, 
+                          TypeDesc *fmt);
     
     
     /****************************
@@ -209,7 +71,7 @@ extern "C" {
      *
      ****************************/
     
-    int ImageInputCreate (ImageInput **iiptr,
+    void ImageInputCreate (ImageInput **iiptr,
 		       char *filename,
 		       char *plugin_searchpath);
 
@@ -238,8 +100,8 @@ extern "C" {
         
     bool ImageOutput_open (ImageOutput *imageOutput,
                            const char *name, 
-                           const struct CImageSpec *newspec,
-                           enum COpenMode mode);
+                           const ImageSpec *newspec,
+                           enum OpenMode mode);
 #ifdef __cplusplus   
 }
 #endif
