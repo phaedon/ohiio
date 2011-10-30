58caa95f6e27ae52c37d2ff1b721d04f29612e05
Phaedon Sinis <phaedonsinis@gmail.com>
2011-10-30 13:53:20 -0700

    filename change

--- src/bindings.hs
+++ src/bindings.hs
@@ -5,7 +5,7 @@ module Ohiio where
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
-import Data.Word
+--import Data.Word
 
 #include "./ohiio.h"
 
@@ -50,13 +50,53 @@ newtype BaseType = BaseType { btype :: CInt }
      lastbase = LASTBASE
 } 
 
-foreign import ccall "ImageSpecCreate_1"
-        c_ImageSpecCreate :: CInt -> CInt -> CInt -> Ptr TypeDesc -> IO (Ptr ImageSpec)
+
+{-
+ class ImageSpec
+-}
+
+foreign import ccall unsafe "ImageSpecCreate_0"
+        c_ImageSpecCreate_0 :: Ptr TypeDesc -> IO (Ptr ImageSpec)
+
+foreign import ccall unsafe "ImageSpecCreate_1"
+        c_ImageSpecCreate_1 :: CInt -> CInt -> CInt -> Ptr TypeDesc -> IO (Ptr ImageSpec)
+
+foreign import ccall unsafe "ImageSpec_width"
+        c_ImageSpec_width :: Ptr ImageSpec -> IO CInt
+
+foreign import ccall unsafe "ImageSpec_height"
+        c_ImageSpec_height :: Ptr ImageSpec -> IO CInt
+
+foreign import ccall unsafe "ImageSpec_nchannels"
+        c_ImageSpec_nchannels :: Ptr ImageSpec -> IO CInt
 
 
+{-
+ class ImageInput
+-}
 
-foreign import ccall "ImageOutputCreate"
---        c_ImageOutputCreate :: Ptr CChar -> Ptr CChar -> Ptr ImageOutput
+foreign import ccall unsafe "ImageInputCreate"
+        c_ImageInputCreate :: CString -> CString -> IO (Ptr ImageInput)
+
+foreign import ccall unsafe "ImageInput_open"
+        c_ImageInput_open :: Ptr ImageInput -> CString -> Ptr ImageSpec -> IO Bool
+
+foreign import ccall unsafe "ImageInput_close"
+        c_ImageInput_close :: Ptr ImageInput -> IO Bool
+
+foreign import ccall unsafe "ImageInput_read_image_0"
+        c_ImageInput_read_image_0 :: Ptr ImageInput -> BaseType -> Ptr Word8 -> IO Bool
+
+foreign import ccall unsafe "ImageInput_read_image_1"
+        c_ImageInput_read_image_1 :: Ptr ImageInput -> Ptr Float -> IO Bool
+
+
+{-
+ class ImageOutput
+-}
+
+
+foreign import ccall unsafe "ImageOutputCreate"
         c_ImageOutputCreate :: CString -> CString -> IO (Ptr ImageOutput)
 
 foreign import ccall unsafe "ImageOutput_open"
